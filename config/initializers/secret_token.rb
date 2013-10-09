3f572a2d0ffe9753236592631e44d5722304e785
kmazanec <k.mazanec@gmail.com>
2013-10-09 17:40:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkingDangerMap::Application.config.secret_key_base = 'a5a09de49e4bc9d81ce68e70f215b5c398ecd39a2857cafec86f698d1a011f2b3fc578b77e6f5a124375e8c7b4707647fffb0fb3da17281d39758ddaa1ac5b25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ParkingDangerMap::Application.config.session_store :cookie_store, key: '_parking-danger-map_session'
