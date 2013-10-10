827730a6708449d5a30368b9f1909b2010e86daf
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 14:48:48 -0500

    model tests and validations for tickets

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -9,8 +9,8 @@ describe Ticket do
     it { should belong_to(:user) }
     it { should belong_to(:location) }
 
-
-
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:location_id) }
   end
 
 
