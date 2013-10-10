6e0544950edf175fcd1d05be16e1e2c6a0b31c48
Apprentice <apprentice@dbc-chicago-001.local>
2013-10-10 16:17:49 -0500

    added get request for map_data

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,4 +4,14 @@ class MapsController < ApplicationController
     @ticket = Ticket.new
   end
 
+  def map_data
+    @locations = Location.all
+    content_type :json
+    {locations:  
+    @locations.map do |location|
+      {latitude: location.latitude, longitude: location.longitude}
+    end
+    }.to_json
+  end
+
 end
