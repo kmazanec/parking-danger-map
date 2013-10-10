57f10c5b656f892e6db0da66eeac5c3440cc1a4c
Apprentice <apprentice@dbc-chicago-001.local>
2013-10-09 22:06:23 -0500

    sessions files

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,9 @@
+module SessionsHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def logged_in?
+    return true if session[logged_in?] = true
+end
