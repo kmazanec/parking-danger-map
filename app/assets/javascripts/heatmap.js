0fc05f491908d32b2d2968c9397d04b1a7ed3f71
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-13 18:19:47 -0500

    added home button

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,6 +1,7 @@
 // Adding 500 Data Points
 var map, heatmap, parkingData;
 var pointArray = [];
+var home = new google.maps.LatLng(40.7833, -73.9667);
 
 
 function convertResponseToLatLong(response){
@@ -41,11 +42,46 @@ function updateMap(){
   });
 }
 
+function HomeControl(controlDiv, map) {
+
+  // Set CSS styles for the DIV containing the control
+  // Setting padding to 5 px will offset the control
+  // from the edge of the map
+  controlDiv.style.padding = '5px';
+
+  // Set CSS for the control border
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = 'white';
+  controlUI.style.borderStyle = 'solid';
+  controlUI.style.borderWidth = '2px';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to set the map to Home';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior
+  var controlText = document.createElement('div');
+  controlText.style.fontFamily = 'Arial,sans-serif';
+  controlText.style.fontSize = '12px';
+  controlText.style.paddingLeft = '4px';
+  controlText.style.paddingRight = '4px';
+  controlText.innerHTML = '<b>Home</b>';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to
+  // Chicago
+  google.maps.event.addDomListener(controlUI, 'click', function() {
+    map.setCenter(home);
+  });
+
+}
+
+
 
 function initialize() {
   var mapOptions = {
     zoom: 12,
-    center: new google.maps.LatLng(40.7833, -73.9667),
+    center: home,
     mapTypeId: google.maps.MapTypeId.MAP
   };
 
@@ -80,6 +116,14 @@ function initialize() {
     clearTimeout(moving);
     moving = setTimeout("updateMap()", 1200);
   });
+
+  var homeControlDiv = document.createElement('div');
+  var homeControl = new HomeControl(homeControlDiv, map);
+
+  homeControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
